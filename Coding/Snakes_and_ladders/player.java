8f798846e9912918303883729b3c2bb450c6211a
karan8404 <114310500+karan8404@users.noreply.github.com>
2022-11-01 00:47:51 +0530

    added java files

---
+++ Coding/Snakes_and_ladders/player.java
@@ -0,0 +1,10 @@
+public class player{
+    int pos;
+    boolean finished;
+    boolean started;
+    player(){
+        pos=0;
+        started=false;
+        finished=false;
+    }
+}
---
+++ Coding/Snakes_and_ladders/random.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class random {
+   static double rand(int lb,int ub){
+       ub++;
+       double size=ub-lb;
+       return Math.floor(Math.random()*size)+lb;
+   }
+   
+   public static void main(String args[])throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the lower bound and upper bound");
+        int lb=Integer.parseInt(br.readLine());
+        int ub=Integer.parseInt(br.readLine());
+        System.out.println("press enter to generate next number or any character to terminate");
+        double r;
+        do{
+            r=rand(lb,ub);
+            System.out.print(r);
+        }
+        while(br.readLine().isEmpty());
+    }
+}
